94d8ff4db2674407c9ce6769928d440930c1562e
hoangnam <hoangnamchelsea2001@gmail.com>
2023-08-08 16:13:59 +0700

    delete config

--- src/utils/config.tsx
+++
@@ -1,5 +0,0 @@
-export const URL_LOGIN_POST = 'http://103.160.2.183:5001/api/auth/login';
-export const URL_REFRESH_TOKEN_POST = 'http://103.160.2.183:5001/api/auth/refresh-tokens';
-export const URL_LOGOUT_POST = 'http://103.160.2.183:5001/api/auth/logout';
-export const URL_GREEN_HOUSE = 'http://103.160.2.183:5001/api/greenhouses';
-export const URL_RELAYS = 'http://103.160.2.183:5001/api/relays/';
